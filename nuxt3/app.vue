c42c84b209986c6eae48451eec8399bf8cb46676
Itsuki Sakitsu <itsuki.sakitsu@gmail.com>
2023-08-13 16:44:16 +0900

    improved app.vue

--- nuxt3/app.vue
+++ nuxt3/app.vue
@@ -20,16 +20,30 @@
     const { data } = await useFetch('/api/jwt')
     jwt.value = data.value || ''
   }
+
+  const config = useRuntimeConfig()
   
 </script>
 
 <template>
   <div>
-    <div><button @click="post">post</button></div>
-    <div>Post state: {{ resDataSuccess }}</div>
-    <div><button @click="get">get</button></div>
-    <div>Get Data: {{ JSON.stringify(resData) }}</div>
-    <div><button @click="getJwt">get</button></div>
+    <div>
+      <h2>sessions</h2>
+      <div><button @click="post">post</button></div>
+      <div>Post state: {{ resDataSuccess }}</div>  
+      <div><button @click="get">get</button></div>
+      <div>Get Data: {{ JSON.stringify(resData) }}</div>
+    </div>
+    
+    <div>
+      <h2>JWT</h2>
+      <div><button @click="getJwt">get</button></div>
     <div>Get Data: {{ JSON.stringify(jwt) }}</div>
+    </div>
+    
+    <div>
+      <h2>Runtime config in client side</h2>
+      <p>{{ JSON.stringify(config) }}</p>
+    </div>
   </div>
 </template>
