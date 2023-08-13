4c473450dde12f85e5bf79c3b0e3ea6eb303db50
Itsuki Sakitsu <itsuki.sakitsu@gmail.com>
2023-08-13 20:49:12 +0900

    disable ssr

--- nuxt3/nuxt.config.ts
+++ nuxt3/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
   runtimeConfig: {
     SUPABASE_JWT_SECRET: process.env.SUPABASE_JWT_SECRET,
     public: {
